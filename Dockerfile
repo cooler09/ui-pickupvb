1c3d92dce6fc14cb603946e5076293322bf22133
Zachary Lockhart <zacharyjordan82@gmail.com>
2022-10-06 20:58:18 -0400

    fixing a few build files

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@
 FROM node:latest as node
 WORKDIR /app
 COPY . .
-RUN npm install
-RUN npm run build --prod
 #stage 2
 FROM nginx:alpine
 COPY --from=node /app/dist/ui-pickupvb /usr/share/nginx/html
--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 if ! hash jq
 then
-    apt-get install jq
+    sudo apt-get install jq
 fi
 
 npm run build --prod
 version=$(jq -r .version ../package.json)
 
-docker build -t cooler09/ui-pickupvb:$version ../.
-docker push cooler09/ui-pickupvb:$version
+sudo docker build -t cooler09/ui-pickupvb:$version -t cooler09/ui-pickupvb:latest ../.
+sudo docker push cooler09/ui-pickupvb:$version
+sudo docker push cooler09/ui-pickupvb:latest
