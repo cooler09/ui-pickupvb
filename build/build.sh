cfaf9c6bc692c190ece226648c882bb8c6de3627
Zachary Lockhart <zacharyjordan82@gmail.com>
2022-10-06 20:33:35 -0400

    adding build script

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+if ! hash jq
+then
+    apt-get install jq
+fi
+
 npm run build --prod
-version=$(jq .version ../package.json)
+version=$(jq -r .version ../package.json)
 
 docker build -t cooler09/ui-pickupvb:$version ../.
 docker push cooler09/ui-pickupvb:$version
