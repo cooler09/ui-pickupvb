2f3aa19415f92de5a45633a904d6fe58484e52f1
Zachary Lockhart <zacharyjordan82@gmail.com>
2023-02-12 11:14:36 -0500

    version and http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://pickupvb.com:5000/",
+  apiUrl: "http://pickupvb.com:5000/",
   appVersion: require('../../package.json').version,
 };
